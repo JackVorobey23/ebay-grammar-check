f8713dab7fc2ea7cca4c3a3c5de45d022dcef1a7
Yevhenii <eygehavv27@gmail.com>
2023-12-10 00:44:34 +0200

    feat: add grammar checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
 import { useState } from "react";
-import useGrammar from "./hooks/useGrammar";
+import getSpellChecking from "./helpers/getSpellChecking";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const grammarResult = useGrammar();
+  const [grammarResult, setGrammarResult] = useState("");
+  const [inputText, setInputText] = useState("");
 
+  console.log(import.meta.env.VITE_SPELL_CHECKER_API_KEY);
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Ebay grammar checker</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          text for checking:
+          <input onChange={(e) => setInputText(e.target.value)} />
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
-      <div>
-        <h1>Grammar Check Result:</h1>
-        <pre>{grammarResult}</pre>
+        <p>{grammarResult == "" ? "\n" : `checking result: ${grammarResult}`}</p>
+        <button
+          onClick={() => {
+            const result = getSpellChecking(inputText)
+              .then((r) => setGrammarResult(r))
+              .catch(() => setGrammarResult("nigga"));
+            console.log(result);
+          }}
+        >
+          Check text
+        </button>
       </div>
     </>
   );
---
+++ src/helpers/getSpellChecking.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const getSpellChecking = (query: string) => {
+  const myHeaders = new Headers();
+  myHeaders.append("apikey", 'import.meta.env.VITE_SPELL_CHECKER_API_KEY');
+
+  const requestOptions: RequestInit = {
+    method: "GET",
+    redirect: "follow",
+    headers: myHeaders,
+  };
+
+  return fetch(
+    `https://api.apilayer.com/spell/spellchecker?q=${query}`,
+    requestOptions
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.text();
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+      throw new Error("Failed to fetch data");
+    });
+};
+
+export default getSpellChecking;
